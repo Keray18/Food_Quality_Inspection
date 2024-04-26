274c779fcac49b2b1e40acc315a8ae0cd6c183dc
Keray18 <keshavbahoray18@gmail.com>
2024-04-26 14:17:56 +0530

    made some changes

--- modelApi/predict_pipeline.py
+++ modelApi/predict_pipeline.py
@@ -1,19 +1,14 @@
 import os
 import pathlib
-import numpy as np  
-from PIL import Image 
-from io import BytesIO 
+import numpy as np
+from PIL import Image
+from io import BytesIO
 import pickle
 import dvc.api
 
 import tensorflow as tf
 from tensorflow.keras.preprocessing import image
 from tensorflow.keras.utils import get_file
-
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
-
-
 
 
 def preprocessing(filename) -> np.ndarray:
@@ -27,11 +22,8 @@ def prediction(filename, model, class_name):
     img_array = preprocessing(filename)
     img_array = np.expand_dims(img_array, axis=0)
 
-    
     predictions = model.predict(img_array)
     predicted_class_index = np.argmax(predictions[0])
-
-    
 
     predicted_class = class_name[predicted_class_index]
     confidence = predictions[0][predicted_class_index]
